b374d185d034c94801d98eaf68eb774cf3f9b227
Kevin Yockey <foddermk@gmail.com>
2018-03-10 10:54:16 -0800

    Added support for using version number as caching.

--- AssetBundleManager/AssetBundleDownloader.cs
+++ AssetBundleManager/AssetBundleDownloader.cs
@@ -11,6 +11,7 @@ namespace AssetBundles
     {
         public string BundleName;
         public Hash128 Hash;
+        public uint Version;
         public Action<AssetBundle> OnComplete;
     }
 
@@ -19,6 +20,8 @@ namespace AssetBundles
         private const int MAX_RETRY_COUNT = 3;
         private const float RETRY_WAIT_PERIOD = 1;
         private const int MAX_SIMULTANEOUS_DOWNLOADS = 4;
+
+        private static readonly Hash128 DEFAULT_HASH = default(Hash128);
 
         private static readonly long[] RETRY_ON_ERRORS = {
             503 // Temporary Server Error
@@ -73,9 +76,12 @@ namespace AssetBundles
         {
             var uri = baseUri + cmd.BundleName;
             UnityWebRequest req;
-            if (cachingDisabled || cmd.Hash == default(Hash128)) {
+            if (cachingDisabled || (cmd.Version <= 0 && cmd.Hash == DEFAULT_HASH)) {
                 Debug.Log(string.Format("GetAssetBundle [{0}].", uri));
                 req = UnityWebRequest.GetAssetBundle(uri);
+            } else if (cmd.Hash == DEFAULT_HASH) {
+                Debug.Log(string.Format("GetAssetBundle v[{0}] [{1}].", cmd.Version, uri));
+                req = UnityWebRequest.GetAssetBundle(uri, cmd.Version, 0);
             } else {
                 Debug.Log(string.Format("GetAssetBundle [{0}] [{1}].", Caching.IsVersionCached(uri, cmd.Hash) ? "cached" : "uncached", uri));
                 req = UnityWebRequest.GetAssetBundle(uri, cmd.Hash, 0);
@@ -104,11 +110,14 @@ namespace AssetBundles
                 }
             }
 
+            AssetBundle bundle;
+
             if (req.isNetworkError) {
                 Debug.LogError(string.Format("Error downloading [{0}]: [{1}]", uri, req.error));
+                bundle = null;
+            } else {
+                bundle = DownloadHandlerAssetBundle.GetContent(req);
             }
-
-            AssetBundle bundle = ((DownloadHandlerAssetBundle)req.downloadHandler).assetBundle;
 
             if (!req.isNetworkError && !req.isHttpError && string.IsNullOrEmpty(req.error) && bundle == null) {
                 Debug.LogWarning(string.Format("There was no error downloading [{0}] but the bundle is null.  Assuming there's something wrong with the cache folder, retrying with cache disabled now and for future requests...", uri));
